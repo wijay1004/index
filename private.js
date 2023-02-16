6944d4266eba682447437fb62feef32c301a3db1
wijay1004 <125537905+wijay1004@users.noreply.github.com>
2023-02-16 13:27:14 +0700

    Create private.js

---
+++ private.js
@@ -0,0 +1,14 @@
+<div style="display:none">
+<a href="https://testb2b.226ers.com/-/roulette-online/">https://testb2b.226ers.com/-/roulette-online/</a>
+<a href="https://aif.redermio.ru/wp-content/uploads/2023/01/-/live-casino/">https://aif.redermio.ru/wp-content/uploads/2023/01/-/live-casino/</a>
+<a href="http://tracer.polnes.ac.id/-/bacarat-online/">http://tracer.polnes.ac.id/-/bacarat-online/</a>
+<a href="http://ujm.polnes.ac.id/-/sicbo-online/">http://ujm.polnes.ac.id/-/sicbo-online/</a>
+<a href="https://graduation.thebritishcollege.edu.np/-/roulette-online/">https://graduation.thebritishcollege.edu.np/-/roulette-online/</a>
+<a href="https://bog.mul.edu.pk/-/baccarat-online/">https://bog.mul.edu.pk/-/baccarat-online/</a>
+<a href="https://cms.mul.edu.pk/-/sicbo-onliine/">https://cms.mul.edu.pk/-/sicbo-onliine/</a>
+<a href="https://api.e-bridge.edu.sg/-/baccarat-online/">https://api.e-bridge.edu.sg/-/baccarat-online/</a>
+<a href="https://recruit.iiitn.ac.in/-/baccarat-online/">https://recruit.iiitn.ac.in/-/baccarat-online/</a>
+<a href="http://apidelta.sman19sby.sch.id/-/sicbo-online/">http://apidelta.sman19sby.sch.id/-/sicbo-online/</a>
+<a href="https://gajah.alamalami.com/baccarat-online/">https://gajah.alamalami.com/baccarat-online/</a>
+<a href="https://reviewaplikasi.com/-/sicbo-online/">https://reviewaplikasi.com/-/sicbo-online/</a>
+</div>
